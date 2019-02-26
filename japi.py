20237f6ad2fb6ea09249f9cfdc3687d6f57b2c34
Billy Downing <bjamesdowning@gmail.com>
2019-02-26 01:14:36 +0000

    First Commit

---
+++ japi.py
@@ -0,0 +1,23 @@
+import requests
+import urllib
+import json
+
+
+
+def getStockData(symbl):
+    url = "https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols="+symbl+"&apikey=WLQOSD2QF516NC1K"
+    conn = urllib.request.urlopen(url)
+    resp = conn.read().decode()
+    print(resp)
+    json2dict = json.loads(resp)
+    print("The current price of {} is: ${}".format(symbl, json2dict["Stock Quotes"][0]["2. price"]))
+
+
+if __name__ == "__main__":
+    f = open("japi.out", "w+")
+    while True:
+        answer = input("Enter Stock Ticker (enter quit to exit): ")
+        if answer != 'quit':
+             getStockData(answer)
+        else:
+            break
